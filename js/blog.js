a16ca885ef43af5b2c7f2abcc460fea985b5b0ff
Sarfraz <sarfraznawaz2005@gmail.com>
2015-04-08 00:17:51 +0500

    updated blog

--- js/blog.js
+++ js/blog.js
@@ -5,19 +5,6 @@
 $(function () {
     // enable syntax highlighting
     hljs.initHighlightingOnLoad();
-    
-    // add line numbering to code
-    $('pre code').each(function(){
-        var lines = $(this).text().split('\n').length - 1;
-        var $numbering = $('<ul/>').addClass('pre-numbering');
-        $(this)
-            .addClass('has-numbering')
-            .parent()
-            .append($numbering);
-        for(i=1;i<=lines;i++){
-            $numbering.append($('<li/>').text(i));
-        }
-    });    
 
     // lowercase link hrefs
     $('a.lowercase').each(function () {
